a5585b80949eb1813dfde938e3d0371bd4ecc73d
Akku1508 <122289440+Akku1508@users.noreply.github.com>
2023-01-21 10:20:27 +0530

    submitting A2_Q4

---
+++ A2_Q4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int n;
+    cin >> n;
+    int i,j;
+    for(i=1;i<=n+1;i++)
+    {
+       for(j=1;j<=(n+1);j++)
+       {
+        if (j>=i)
+        {
+            cout<<"*\t";
+        }
+        else
+        {
+            cout<<"\t";
+        }
+       }
+
+        cout<<"\n";
+       
+       
+    }
+    
+}
