6de89227bd396d27f9094c0cb8b7b5e0d9a53d2f
Akku1508 <122289440+Akku1508@users.noreply.github.com>
2023-01-29 08:55:31 +0530

    submitting A2_Q14

---
+++ A2_Q14.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(int agrc, char** argv)
+{
+    int n;
+    cin >> n;
+    int i,k;
+    for(i=1;i<=10;i++)
+    {
+        k=n*i;
+        cout<<n<<" * "<<i<<" = "<<k<<"\n";
+    }
+        
+}
